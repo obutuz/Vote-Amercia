d0c9b810d48d71efc01517bb8dc86aa36f6a2998
Chris Becker <Cbeck527@gmail.com>
2012-10-11 15:15:32 -0400

    Lets see if this works as a copy cat

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 Vote-Amercia
 ============
+
+Sms voting app built for the #ElectionClass primary election.
+
+Uses PHP, Twilio, and a little magic.
+
+by Andrew Bauer and Chris Becker
--- sms.php
+++ sms.php
@@ -1,7 +1,12 @@
 <?php
+    // grab the post data and send to andrew's fuction
+
+    $user_number = $_POST['From'];
+    $message = $_POST['Body'];
+
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Sms>Test reply!</Sms>
+    <Sms>This should be your message... <?php echo $message ?></Sms>
 </Response>
